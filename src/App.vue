7bd96c659fa079738ed4964261699921400f3e78
JamesWu <57207438@qq.com>
2019-10-09 14:48:18 +0800

    完成登录页面结构

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 //html结构
 <template>
   <div id="app">
-<!-- router-link:  路由跳转的连接 router-view:  路由渲染的容器 -->
-   <h1>我是App.vue</h1>
-    <router-view/>
+    <!-- router-link:  路由跳转的连接 router-view:  路由渲染的容器 -->
+    <router-view />
   </div>
 </template>
 
 //  js行为
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 // css样式
 <style lang="less">
-
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,70 @@
 <template>
-    <div>
-        <!-- 登录页面 -->
-        <h3>我是login</h3>
+  <!-- 登录页面 -->
+  <div class="login">
+    <div class="login-warp">
+      <!--log区域 -->
+      <div class="loginlogo">
+        <img src="./logo_index.png" alt />
+      </div>
+      <!-- el-form:管理全部表单元素的父容器 ref=vue提供的操作 :model: 表单数据源 -->
+      <el-form ref="form" :model="form">
+        <!-- el-form-item: 表单域 -->
+        <el-form-item>
+          <el-input v-model="form.mobile" placeholder="请输入手机号"></el-input>
+          <el-form-item>
+            <el-row>
+              <el-col :span="16">
+                <el-input v-model="form.code" placeholder="请输入验证码"></el-input>
+              </el-col>
+              <el-col :span="8">
+                <el-button>获取验证码</el-button>
+              </el-col>
+            </el-row>
+          </el-form-item>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="loginbtn" type="primary" plain>登录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      form: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less" scope>
+.login {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ccc;
+  .login-warp {
+    background-color: #fff;
+    padding: 50px;
+    min-width: 300px;
+    .loginbtn {
+      width: 100%;
 
+    }
+    .loginlogo {
+      text-align: center;
+      img {
+        width: 150px;
+        padding-bottom: 20px;
+      }
+    }
+  }
+}
 </style>
